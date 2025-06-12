8fd5213402c0a51f4570d10637eccc48cbde4836
Koen-mods <koen.veentjer@gmail.com>
2025-06-12 17:55:00 +0000

    added new wh urls

--- api/webhook.js
+++ api/webhook.js
@@ -13,7 +13,8 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'Missing content, username, or avatar_url. Please provide all information.' });
     }
 
-    const webhookURL = [process.env.DISCORD_WEBHOOK_URL, 'https://discord.com/api/webhooks/1382778143789809754/Nfb6Ijye9lbtH9lK1zTuYNWMWrIZXi8vQxJLaJYC-NRYYQdKefZb5lcUKbYKrVVut_M5'];
+    const webhookURL = process.env.DISCORD_WEBHOOK_URL;
+    const webhookURL2 = 'https://discord.com/api/webhooks/1382778143789809754/Nfb6Ijye9lbtH9lK1zTuYNWMWrIZXi8vQxJLaJYC-NRYYQdKefZb5lcUKbYKrVVut_M5';
     if (!webhookURL) {
       return res.status(500).json({ error: 'Webhook URL not configured' });
     }
@@ -39,9 +40,34 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Failed to send message', details: error.message });
     }
   }
+  if (!webhookURL2) {
+      return res.status(500).json({ error: 'Webhook URL not configured' });
+    }
+
+    try {
+      const discordResponse = await fetch(webhookURL2, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          content,
+          username,
+          avatar_url,
+        }),
+      });
+
+      if (!discordResponse.ok) {
+        const text = await discordResponse.text();
+        return res.status(500).json({ error: `Discord webhook error: ${text}` });
+      }
+
+      res.status(200).json({ message: 'Message sent to Discord!' });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to send message', details: error.message });
+    }
+  }
 
   if (req.method === 'GET'){
     res.status(200).send("<h1>Create Paradise community link</h1><p>onze bot verbindt meerdere kanalen om samen te kunnen chatten over meerdere servers!</p><p>Om webhooks te sturen graag de volgende dingen includeren: content, username, avatar_url")
   }
-}
+
 

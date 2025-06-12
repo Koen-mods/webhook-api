27cd86dd1adcbb9ce3296d7aa3d65fb7b7c52494
Koen-mods <koen.veentjer@gmail.com>
2025-06-12 18:00:04 +0000

    updated

--- api/webhook.js
+++ api/webhook.js
@@ -40,34 +40,9 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Failed to send message', details: error.message });
     }
   }
-  if (!webhookURL2) {
-      return res.status(500).json({ error: 'Webhook URL not configured' });
-    }
-
-    try {
-      const discordResponse = await fetch(webhookURL2, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          content,
-          username,
-          avatar_url,
-        }),
-      });
-
-      if (!discordResponse.ok) {
-        const text = await discordResponse.text();
-        return res.status(500).json({ error: `Discord webhook error: ${text}` });
-      }
-
-      res.status(200).json({ message: 'Message sent to Discord!' });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to send message', details: error.message });
-    }
-  }
 
   if (req.method === 'GET'){
-    res.status(200).send("<h1>Create Paradise community link</h1><p>onze bot verbindt meerdere kanalen om samen te kunnen chatten over meerdere servers!</p><p>Om webhooks te sturen graag de volgende dingen includeren: content, username, avatar_url")
+    res.status(200).send("<h1>Create Paradise community link</h1><p>onze bot verbindt meerdere kanalen om samen te kunnen chatten over meerdere servers!</p><p>Om webhooks te sturen graag de volgende dingen includeren: content, username, avatar_url");
   }
-
+}
 

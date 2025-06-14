660acee4f89738ced182fdf786f21801c893e7cf
Koen-mods <koen.veentjer@gmail.com>
2025-06-14 11:53:17 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -13,56 +13,29 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'Missing content, username, or avatar_url. Please provide all information.' });
     }
 
-    const webhookURL = process.env.DISCORD_WEBHOOK_URL;
-    const webhookURL2 = 'https://discord.com/api/webhooks/1382778143789809754/Nfb6Ijye9lbtH9lK1zTuYNWMWrIZXi8vQxJLaJYC-NRYYQdKefZb5lcUKbYKrVVut_M5';
-    if (!webhookURL) {
-      return res.status(500).json({ error: 'Webhook URL not configured' });
-    }
+    const webhookURLs = [process.env.DISCORD_WEBHOOK_URL, 'https://discord.com/api/webhooks/1382778143789809754/Nfb6Ijye9lbtH9lK1zTuYNWMWrIZXi8vQxJLaJYC-NRYYQdKefZb5lcUKbYKrVVut_M5'];
+    for (let i = 0; i < len(webhookURLs); i++)
 
-    try {
-      const discordResponse = await fetch(webhookURL2, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          content,
-          username,
-          avatar_url,
-        }),
-      });
+      try {
+        const discordResponse = await fetch(webhookURLs[i], {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            content,
+            username,
+            avatar_url,
+          }),
+        });
 
-      if (!discordResponse.ok) {
-        const text = await discordResponse.text();
-        return res.status(500).json({ error: `Discord webhook error: ${text}` });
+        if (!discordResponse.ok) {
+          const text = await discordResponse.text();
+          return res.status(500).json({ error: `Discord webhook error: ${text}` });
+        }
+
+        res.status(200).json({ message: 'Message sent to Discord!' });
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to send message', details: error.message });
       }
-
-      res.status(200).json({ message: 'Message sent to Discord!' });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to send message', details: error.message });
     }
   }
-  if (!webhookURL2) {
-      return res.status(500).json({ error: 'Webhook URL not configured' });
-  }
-
-    try {
-      const discordResponse = await fetch(webhookURL2, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          content,
-          username,
-          avatar_url,
-        }),
-      });
-
-      if (!discordResponse.ok) {
-        const text = await discordResponse.text();
-        return res.status(500).json({ error: `Discord webhook error: ${text}` });
-      }
-
-      res.status(200).json({ message: 'Message sent to Discord!' });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to send message', details: error.message });
-    }
 }
-

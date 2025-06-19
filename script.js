35dde91fd087883c42b6b6523fbaa301d2dedebd
Koen-mods <koen.veentjer@gmail.com>
2025-06-19 20:24:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let people = 439;
 const p = document.getElementById("aantal-mensen");
 
 document.querySelector("#interresse").addEventListener("click", e => { 
-  gsap.fromTo(".box", {scale: 1}, {scale: 0.75, duration: 0.25, yoyo: true, repeat: 1, overwrite: true});
+  gsap.fromTo("#box", {scale: 1}, {scale: 0.75, duration: 0.25, yoyo: true, repeat: 1, overwrite: true});
 
 function interesse() {
   people++

d565010b37531a19d7d9ef078e63c03ba2f5f148
Koen-mods <koen.veentjer@gmail.com>
2025-06-19 20:32:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const p = document.getElementById("aantal-mensen");
 function interesse() {
   people++
   p.innerHTML = `Geïnteresseerde mensen: ${people}`;
-  gsap.fromTo("#interresse", {scale: 1}, {scale: 0.75, duration: 0.25, yoyo: true, repeat: 1, overwrite: true});
+  p.style.transfrom = scale(1.2);
 }

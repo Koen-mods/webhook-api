0f35d4b5801ac2f9155dac637230992a7f806a03
Koen-mods <koen.veentjer@gmail.com>
2025-06-22 12:20:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let people = 439;
 const p = document.getElementById("aantal-mensen");
+const b = document.getElementById("interresse");
   
 
 function interesse() {
   people++
   p.innerHTML = `Geïnteresseerde mensen: ${people}`;
-  p.style.transfrom = scale(1.2);
+  b.style.transfrom = "scale(1.2)";
 }
